ab2bd77b32147ca9157f48ad263546e8b67d75fc
Max Monis <mmonis77@gmail.com>
2023-12-03 11:35:44 -0500

    Update landing page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,9 @@ import {SubjectLinks} from "./components/SubjectLinks"
 export default function HomePage() {
   return (
     <main className="flex h-full w-full flex-col items-center px-6 text-center">
-      <h1 className="text-2xl font-bold sm:text-3xl">Random Samplings</h1>
-      <h2 className="mb-20 mt-4 text-xl font-bold sm:text-2xl">
+      <h1 className="mb-20 text-2xl font-bold sm:text-3xl">
         Margaret&apos;s Musings
-      </h2>
+      </h1>
       <SubjectLinks />
     </main>
   )
