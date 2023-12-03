76365193d8f1e2b73587ed618ba52b26b618c3fd
Max Monis <mmonis77@gmail.com>
2023-12-03 00:27:37 -0500

    Add support for nested images in article

--- src/app/[subject]/[slug]/page.tsx
+++ src/app/[subject]/[slug]/page.tsx
@@ -30,13 +30,23 @@ export default async function ArticlePage({
       </h1>
       <Image height={191} priority width={286} {...{alt, src}} />
       <div className="my-10 flex flex-col items-center gap-4">
-        {text
-          .split(/\r|\n/)
-          .filter(Boolean)
-          .map((paragraph, i) => (
-            <p dangerouslySetInnerHTML={{__html: paragraph}} key={i} />
-          ))}
+        {text.split(/\r|\n/).map((text, i) => (
+          <NestedContent key={i} {...{text}} />
+        ))}
       </div>
     </div>
   )
+}
+
+function NestedContent({text}: {text: string}) {
+  if (!text) {
+    return null
+  }
+
+  const [, alt, src] = text.match(/\!\[(.*?)\]\((.*?)\)/) ?? []
+  if (alt && src) {
+    return <Image height={191} width={286} {...{alt, src}} />
+  }
+
+  return <p dangerouslySetInnerHTML={{__html: text}} />
 }
