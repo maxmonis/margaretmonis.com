986e801c358233d18f21819868e7295931771376
Max Monis <mmonis77@gmail.com>
2023-12-03 00:27:04 -0500

    Reduce size of article image, refactor page

--- src/app/[subject]/[slug]/page.tsx
+++ src/app/[subject]/[slug]/page.tsx
@@ -18,26 +18,24 @@ export default async function ArticlePage({
   }
 
   const {
-    image: {alt, height, url, width},
+    image: {alt, url: src},
     text,
     title,
   } = article
 
   return (
-    <div className="flex flex-col items-center px-6">
-      <div className="max-w-xl">
-        <h1 className="mb-10 text-center text-2xl font-bold sm:text-3xl">
-          {title}
-        </h1>
-        <Image priority src={url} {...{alt, height, width}} />
-        <div className="mt-10 flex flex-col gap-4">
-          {text
-            .split(/\r|\n/)
-            .filter(Boolean)
-            .map((paragraph, i) => (
-              <p dangerouslySetInnerHTML={{__html: paragraph}} key={i} />
-            ))}
-        </div>
+    <div className="mx-auto flex max-w-xl flex-col items-center px-6">
+      <h1 className="mb-10 text-center text-2xl font-bold sm:text-3xl">
+        {title}
+      </h1>
+      <Image height={191} priority width={286} {...{alt, src}} />
+      <div className="my-10 flex flex-col items-center gap-4">
+        {text
+          .split(/\r|\n/)
+          .filter(Boolean)
+          .map((paragraph, i) => (
+            <p dangerouslySetInnerHTML={{__html: paragraph}} key={i} />
+          ))}
       </div>
     </div>
   )
