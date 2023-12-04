4b2d4d2b56b1018ad87ecf6f618a6056c47a39de
Max Monis <mmonis77@gmail.com>
2023-12-04 00:10:43 -0500

    Minor refactor of sitemap for simplicity and clarity

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,36 +3,32 @@ import {MetadataRoute} from "next"
 import {subjects} from "./[subject]/constants"
 import {loadArticles} from "./[subject]/functions"
 
-export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
-  const articleRoutes: MetadataRoute.Sitemap = []
-  for (const subject of subjects) {
-    const articles = await loadArticles(subject)
-    for (const {slug} of articles) {
-      const route = {
-        changeFrequency: "weekly",
-        lastModified: new Date(),
-        priority: 0.5,
-        url: `${siteUrl}/${subject}/${slug}`,
-      } as const
-      articleRoutes.push(route)
-    }
-  }
-  return [
+export default async function Sitemap() {
+  const lastModified = new Date()
+  const sitemap: MetadataRoute.Sitemap = [
     {
       changeFrequency: "yearly",
-      lastModified: new Date(),
+      lastModified,
       priority: 1,
       url: siteUrl,
     },
-    ...subjects.map(
-      subject =>
-        ({
-          changeFrequency: "monthly",
-          lastModified: new Date(),
-          priority: 0.8,
-          url: `${siteUrl}/${subject}`,
-        }) as const,
-    ),
-    ...articleRoutes,
   ]
+  for (const subject of subjects) {
+    sitemap.push({
+      changeFrequency: "monthly",
+      lastModified,
+      priority: 0.8,
+      url: `${siteUrl}/${subject}`,
+    })
+    const articles = await loadArticles(subject)
+    for (const {slug} of articles) {
+      sitemap.push({
+        changeFrequency: "weekly",
+        lastModified,
+        priority: 0.5,
+        url: `${siteUrl}/${subject}/${slug}`,
+      })
+    }
+  }
+  return sitemap
 }
