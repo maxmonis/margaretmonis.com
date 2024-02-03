be52992eb871033264f267c3603dcb995bf7cf2a
Max Monis <mmonis77@gmail.com>
2024-02-03 15:56:01 -0500

    Move recently published above subjects on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,17 @@ export default async function HomePage() {
         Margaret&apos;s Musings
       </h1>
       <h2 className="mb-6 text-center text-xl font-bold sm:text-2xl">
-        Subjects
-      </h2>
-      <SubjectLinks priority />
-      <h2 className="mb-6 mt-20 text-center text-xl font-bold sm:text-2xl">
         Recently Published
       </h2>
-      <div className="flex max-w-5xl flex-wrap justify-center gap-6">
+      <div className="mb-20 flex max-w-5xl flex-wrap justify-center gap-6">
         {allArticles.map(article => (
           <ArticleLink key={article.slug} priority {...{article}} />
         ))}
       </div>
+      <h2 className="mb-6 text-center text-xl font-bold sm:text-2xl">
+        Subjects
+      </h2>
+      <SubjectLinks priority />
     </main>
   )
 }
